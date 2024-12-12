660d6b798832eae8846523a31adde3640b55a2a5
SeongGwang <seonggwangdev@gmail.com>
2024-12-12 15:33:44 +0900

    👷 chore: Config 및 build.gradle 정리

---
+++ src/main/java/com/mulmeong/chat/common/config/CorsConfig.java
@@ -0,0 +1,19 @@
+package com.mulmeong.chat.common.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+public class CorsConfig implements WebFluxConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedHeaders("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+                .allowCredentials(true);
+    }
+}
